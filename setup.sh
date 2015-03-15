605ab736a7e09d21ed08e9c94930a78d98a36749
Shane O'Grady <shane@ogrady.ie>
2015-03-15 13:34:47 -0300

    Tweak setup script so it doesn't fail

--- setup.sh
+++ setup.sh
@@ -6,7 +6,13 @@ git submodule update --init --recursive
 for name in *; do
   target="$HOME/.$name"
   if [ "$name" != 'setup.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'LICENSE' ]; then
-    echo "Creating $target"
-    ln -s "$PWD/$name" "$target"
+    if [ -e "$target" ]; then
+      if [ ! -L "$target" ]; then
+        echo "WARNING: $target exists but is not a symlink."
+      fi
+    else
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
   fi
 done
