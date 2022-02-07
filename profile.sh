44a446282a3eca613a272bde27f66ec07bd120d3
Shane O'Grady <shane@ogrady.ie>
2022-02-07 14:27:10 -0800

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,4 +1,9 @@
 # source the users bashrc if it exists
-if [ -e "${HOME}/.bashrc" ] ; then
-  source "${HOME}/.bashrc"
+# if [ -e "${HOME}/.bashrc" ] ; then
+#   source "${HOME}/.bashrc"
+# fi
+
+# source the users zshrc if it exists
+if [ -e "$HOME/.config/zsh/.zshrc" ] ; then
+  source "${HOME}/.config/zsh/.zshrc"
 fi
