b101c528b6d1c1432ed4b8fffcd5881690d74f59
Shane O'Grady <shane@ogrady.ie>
2022-02-07 15:53:20 -0800

    refactor: remove unused files

--- bash_profile.sh
+++
@@ -1,9 +0,0 @@
-# source the system wide bashrc if it exists
-if [ -e /etc/bash.bashrc ] ; then
-  source /etc/bash.bashrc
-fi
-
-# source the users bashrc if it exists
-if [ -e "${HOME}/.bashrc" ] ; then
-  source "${HOME}/.bashrc"
-fi
