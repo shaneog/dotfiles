6c6679aeb9e19fb5bc3184fac519d7fa872dadb9
Shane O'Grady <shane@ogrady.ie>
2022-03-06 16:50:11 -0800

    feat: switch from spaceship to starship

--- config/zsh/lib/prompt.zsh
+++ config/zsh/lib/prompt.zsh
@@ -1,11 +1,7 @@
 #!/usr/bin/env zsh
 
-# Spaceship
-zinit load spaceship-prompt/spaceship-prompt
-
-SPACESHIP_GIT_STATUS_PREFIX=" "
-SPACESHIP_GIT_STATUS_SUFFIX=""
-SPACESHIP_GIT_STATUS_ADDED="%F{yellow}•%F{red}"
-SPACESHIP_GIT_STATUS_UNTRACKED="%F{blue}•%F{red}"
-SPACESHIP_GIT_STATUS_DELETED="%F{red}•%F{red}"
-SPACESHIP_GIT_STATUS_MODIFIED="%F{green}•%F{green}"
+# https://starship.rs
+zinit ice lucid as"program" bpick"*aarch64*"  from"gh-r" \
+  atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+  atpull"%atclone" src"init.zsh"
+zinit load starship/starship
