90e5a068b30f56c957a1e7fa4b5264d3f902b377
Shane O'Grady <shane@ogrady.ie>
2024-05-08 12:49:48 -0400

    feat: add node build defs

--- config/zsh/lib/node.zsh
+++ config/zsh/lib/node.zsh
@@ -5,11 +5,14 @@ export NODENV_ROOT="$XDG_DATA_HOME/nodenv"
 # Use XDG_DATA_HOME for npm cache
 export NPM_CONFIG_CACHE="$XDG_DATA_HOME/npm"
 
+# https://github.com/nodenv/node-build-update-defs
+export NODE_BUILD_DEFINITIONS="/opt/homebrew/opt/node-build-update-defs/share/node-build"
+
 # nodenv init -
 if [[ -x "$(command -v nodenv)" ]]; then
   export PATH="$NODENV_ROOT/shims:${PATH}"
   export NODENV_SHELL=zsh
-  source '/opt/homebrew/Cellar/nodenv/1.4.0/libexec/../completions/nodenv.zsh'
+  source '/opt/homebrew/opt/nodenv/completions/nodenv.zsh'
   command nodenv rehash 2>/dev/null
   nodenv() {
     local command
