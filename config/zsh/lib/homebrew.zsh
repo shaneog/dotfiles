36a2a72b75c8ebc60fcfb5c2a0c3f8dcb1390f46
Shane O'Grady <shane@ogrady.ie>
2024-05-08 12:52:14 -0400

    fix: hide homebrew env hints

--- config/zsh/lib/homebrew.zsh
+++ config/zsh/lib/homebrew.zsh
@@ -6,3 +6,5 @@ FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
 
 # Opt-out of homebrew analytics
 HOMEBREW_NO_ANALYTICS=1
+# Don't show homebrew environment hints
+HOMEBREW_NO_ENV_HINTS=1
