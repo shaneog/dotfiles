4919dcc033f4bcb1b6f086d1d473594410d7aae3
Shane O'Grady <shane@ogrady.ie>
2024-05-08 12:51:57 -0400

    fix: correctly setup pyenv

--- config/zsh/lib/python.zsh
+++ config/zsh/lib/python.zsh
@@ -3,11 +3,11 @@
 export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
 
 if [[ -x "$(command -v pyenv)" ]]; then
-  # nodenv init --path
+  # pyenv init --path
   export PATH="$PYENV_ROOT/shims:${PATH}"
-  # nodenv init -
+  # pyenv init -
   export PYENV_SHELL=zsh
-  source '/opt/homebrew/Cellar/pyenv/2.2.4-1/libexec/../completions/pyenv.zsh'
+  source "/opt/homebrew/opt/pyenv/completions/pyenv.zsh"
   command pyenv rehash 2>/dev/null
   pyenv() {
     local command
