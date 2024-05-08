9a840db705348da6f56ff97e7b2911e80394e6b1
Shane O'Grady <shane@ogrady.ie>
2024-05-08 12:53:30 -0400

    fix: direnv platform

--- config/zsh/lib/direnv.zsh
+++ config/zsh/lib/direnv.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-zinit from"gh-r" as"program" bpick"*arm*" mv"direnv* -> direnv" \
+zinit from"gh-r" as"program" bpick"*arm64*" mv"direnv* -> direnv" \
   atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
   pick"direnv" src="zhook.zsh" for \
     direnv/direnv
