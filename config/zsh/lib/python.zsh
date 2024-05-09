98849013cae3b73fd820423da21e4fb2bc34660f
Shane O'Grady <shane@ogrady.ie>
2024-05-09 13:31:11 -0400

    feat: use pyenv-virtualenv

--- config/zsh/lib/python.zsh
+++ config/zsh/lib/python.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# pyenv
 export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
 
 if [[ -x "$(command -v pyenv)" ]]; then
@@ -25,4 +26,21 @@ if [[ -x "$(command -v pyenv)" ]]; then
       ;;
     esac
   }
+fi
+
+# pyenv-virtualenv
+export PATH="$PYENV_ROOT/plugins/pyenv-virtualenv/shims:${PATH}";
+export PYENV_VIRTUALENV_INIT=1;
+_pyenv_virtualenv_hook() {
+  local ret=$?
+  if [ -n "${VIRTUAL_ENV-}" ]; then
+    eval "$(pyenv sh-activate --quiet || pyenv sh-deactivate --quiet || true)" || true
+  else
+    eval "$(pyenv sh-activate --quiet || true)" || true
+  fi
+  return $ret
+};
+typeset -g -a precmd_functions
+if [[ -z $precmd_functions[(r)_pyenv_virtualenv_hook] ]]; then
+  precmd_functions=(_pyenv_virtualenv_hook $precmd_functions);
 fi
