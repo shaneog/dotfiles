de384719aa2d01bd46a12e263654c91c4437ef29
Shane O'Grady <shane@ogrady.ie>
2024-05-09 11:32:04 -0400

    fix: choose tar.gz release asset

--- config/zsh/lib/prompt.zsh
+++ config/zsh/lib/prompt.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # https://starship.rs
-zinit ice as"program" bpick"*aarch64*"  from"gh-r" \
+zinit ice as"program" bpick"*aarch64*.tar.gz"  from"gh-r" \
   atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
   atpull"%atclone" src"init.zsh"
 zinit light starship/starship
