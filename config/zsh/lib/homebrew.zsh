9d495f19418a17ba6a2a200525abe42c0d106d10
Shane O'Grady <shane@ogrady.ie>
2024-05-10 17:08:56 -0400

    fix: use true instead of 1

--- config/zsh/lib/homebrew.zsh
+++ config/zsh/lib/homebrew.zsh
@@ -5,6 +5,6 @@ zinit snippet PZTM::homebrew
 FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
 
 # Opt-out of homebrew analytics
-HOMEBREW_NO_ANALYTICS=1
+HOMEBREW_NO_ANALYTICS=true
 # Don't show homebrew environment hints
-HOMEBREW_NO_ENV_HINTS=1
+HOMEBREW_NO_ENV_HINTS=true
