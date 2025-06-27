28608e8d6d025bf12cb19389040dd6d8819ab3c8
Shane O'Grady <shaneog@users.noreply.github.com>
2025-06-27 08:58:59 -0400

    fix: check for socket file, not regular

--- config/zsh/lib/1password.zsh
+++ config/zsh/lib/1password.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-if [ -f ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ]; then
+if [ -S ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ]; then
   export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 fi
