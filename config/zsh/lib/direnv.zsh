0caafc3426bb627bcde3fdfe42d26b66c6ae53d4
Shane O'Grady <shaneog@users.noreply.github.com>
2026-08-21 13:41:29 -0400

    fix: make direnv's hook generate on a fresh install
    atclone ran ./direnv before pick had made it executable, so it failed
    with permission denied and zhook.zsh was never written. src then had
    nothing to source, leaving direnv with no shell hook at all on a new
    machine until the next zinit update happened to re-run the hook.

--- config/zsh/lib/direnv.zsh
+++ config/zsh/lib/direnv.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 zinit from"gh-r" as"program" bpick"*arm64*" mv"direnv* -> direnv" \
-  atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
+  atclone'chmod u+x ./direnv; ./direnv hook zsh > zhook.zsh' atpull'%atclone' \
   pick"direnv" src="zhook.zsh" for \
   direnv/direnv
